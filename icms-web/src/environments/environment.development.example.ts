7dd6c6de60f1ed48d58bf8eff6d0af0fd3b41907
ijoollee-biyyaa <149656518+ijoollee-biyyaa@users.noreply.github.com>
2026-09-25 16:29:17 +0300

    chore: untrack environment.development.ts, add example file

---
+++ icms-web/src/environments/environment.development.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    apiUrl: '/api',
+    apiBase: 'http://localhost:your-port-number', // Replace 'your-port-number' with the actual port number your backend is running on                                                                                                                                                                                                                                                      
+    districtId: 1,
+    cloudinary:{
+        cloudName:'your-cloud-name', // Replace 'your-cloud-name' with your actual Cloudinary cloud name
+        uploadPreset:'your-upload-preset' // Replace 'your-upload-preset' with your actual Cloudinary upload preset
+    }
+
+}                                                           
--- icms-web/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: '/api',
-  districtId: 1,
-  cloudinary: {
-    cloudName: 'cemrsedb',
-    uploadPreset: 'icms_members',
-  },
-};
